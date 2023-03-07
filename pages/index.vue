b31f1b31775c04fea1852774e24f6ed0a4026eb4
Dmitrii Mazurok <tmazurok@gmail.com>
2023-03-08 01:05:20 +0300

    get audio from random number

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <div class="grid">
         <div>
           <span class="p-float-label">
-            <InputText class="input" id="min" v-model="min" />
+            <InputNumber class="input" id="min" v-model="min" autofocus />
             <label for="min">Min</label>
           </span>
         </div>
@@ -19,6 +19,8 @@
 
       <Button type="submit">Select</Button>
     </form>
+
+    <audio v-show="url.length" :src="url" controls class="audio" />
   </Container>
 </template>
 
@@ -27,15 +29,27 @@ import { ref } from "vue";
 import Container from "src/shared/ui/container.vue";
 import { numbersApi } from "src/shared/api/numbers";
 
-const min = ref(0);
-const max = ref(0);
+const min = ref(1);
+const max = ref(100);
+const url = ref("");
+
+const base64ToBlob = (base64, type) => {
+  const byteString = atob(base64);
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ab], { type });
+};
 
 const onSubmit = async (e) => {
   e.preventDefault();
 
-  numbersApi
-    .getAudio({ min: min.value, max: max.value })
-    .then((data) => console.log(data));
+  const res = await numbersApi.getAudio({ min: min.value, max: max.value });
+  const blob = base64ToBlob(res.audio, "audio/mpeg");
+
+  url.value = URL.createObjectURL(blob);
 };
 </script>
 
@@ -51,5 +65,9 @@ const onSubmit = async (e) => {
 
 .input {
   width: 100%;
+}
+
+.audio {
+  margin-top: 20px;
 }
 </style>
--- server/api/numbers.ts
+++ server/api/numbers.ts
@@ -1,8 +1,29 @@
+import { TextToSpeechClient } from "@google-cloud/text-to-speech";
+import base64 from "base64-js";
+
 export default defineEventHandler(async (event) => {
   const { min, max } = await readBody(event);
 
-  return {
-    min,
-    max,
-  };
+  const number = Math.floor(Math.random() * (max - min + 1)) + min;
+  const audio = await getAudioForNumber(number);
+
+  if (!audio) {
+    return { error: true, audio: null };
+  }
+
+  const str = typeof audio === "string" ? audio : base64.fromByteArray(audio);
+
+  return { error: false, audio: str, number };
 });
+
+async function getAudioForNumber(number: number) {
+  const client = new TextToSpeechClient();
+
+  const [response] = await client.synthesizeSpeech({
+    input: { text: number.toString() },
+    voice: { languageCode: "tr-TR", ssmlGender: "FEMALE" },
+    audioConfig: { audioEncoding: "MP3" },
+  });
+
+  return response.audioContent;
+}
