1c6459c3f70af136776ba9486ccf05ac0248c69d
Dmitrii Mazurok <tmazurok@gmail.com>
2023-03-08 23:10:04 +0300

    add primevue

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -3,10 +3,14 @@ import PrimeVue from "primevue/config";
 import Button from "primevue/button";
 import InputText from "primevue/inputtext";
 import InputNumber from "primevue/inputnumber";
+import ToastService from "primevue/toastservice";
+import Toast from "primevue/toast";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, { ripple: true });
   nuxtApp.vueApp.component("Button", Button);
   nuxtApp.vueApp.component("InputNumber", InputNumber);
   nuxtApp.vueApp.component("InputText", InputText);
+  nuxtApp.vueApp.use(ToastService);
+  nuxtApp.vueApp.component("Toast", Toast);
 });
